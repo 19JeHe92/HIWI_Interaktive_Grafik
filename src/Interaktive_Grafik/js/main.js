2da0b3a558e046d3924c1665496031e88270b6f5
19JeHe92 <jessi_pilgehempel@web.de>
2019-06-18 23:55:17 +0200

    Calculate the sizes of each ar

--- src/Interaktive_Grafik/js/main.js
+++ src/Interaktive_Grafik/js/main.js
@@ -14,3 +14,23 @@ const y = d3.scaleSqrt()
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
 const partition = d3.partition();
+
+const arc = d3.arc()
+    .startAngle(d => x(d.x0))
+    .endAngle(d => x(d.x1))
+    .innerRadius(d => Math.max(0, y(d.y0)))
+    .outerRadius(d => Math.max(0, y(d.y1)));
+
+const middleArcLine = d => {
+    const halfPi = Math.PI/2;
+    const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+    const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+
+    const middleAngle = (angles[1] + angles[0]) / 2;
+    const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+    if (invertDirection) { angles.reverse(); }
+
+    const path = d3.path();
+    path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+    return path.toString();
+};
