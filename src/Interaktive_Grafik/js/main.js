955a883489d74112e44e1ceab20f869166d7b5dd
19JeHe92 <jessi_pilgehempel@web.de>
2019-06-18 23:53:54 +0200

    Init cont vars

--- src/Interaktive_Grafik/js/main.js
+++ src/Interaktive_Grafik/js/main.js
@@ -1,1 +1,16 @@
+const width = 800,
+    height = 800,
+    maxRadius = (Math.min(width, height) / 2) - 50;
 
+const formatNumber = d3.format(',d');
+
+const x = d3.scaleLinear()
+    .range([0, 2 * Math.PI])
+    .clamp(true);
+
+const y = d3.scaleSqrt()
+    .range([maxRadius*.1, maxRadius]);
+
+const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+const partition = d3.partition();
